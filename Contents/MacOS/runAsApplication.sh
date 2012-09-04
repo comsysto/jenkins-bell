85e741db2954f023a9645d5aec4cb35b7bd2aa71
Otto Krammer <otto.krammer@comsysto.com>
2012-09-04 20:01:57 +0200

    fixed command line scripts and app launcher

--- Contents/MacOS/runAsApplication.sh
+++ Contents/MacOS/runAsApplication.sh
@@ -1,18 +1,13 @@
 #!/bin/sh -li
 
-echo "run" > ~/test_app_run
+echo "runAsApplication" > ~/test_app_run
 
 cd "$(dirname $0)"
 DIR=$(pwd)
 
 export BIN_DIR=$DIR/../bin
 
-MY_CMD=$1
-if [ -z "$MY_CMD" ]; then
-    MY_CMD=monitor
-fi
-
 cd $BIN_DIR
 export JAVA_OPTS=-Xdock:name="JenkinsBell"
 
-env groovy main.groovy "$MY_CMD"
+env groovy main.groovy monitor
