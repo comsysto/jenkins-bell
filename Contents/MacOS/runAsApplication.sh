247422af14885afed254554798228f04d6dac825
Otto Krammer <otto.krammer@comsysto.com>
2013-09-11 12:25:23 +0200

    added syslog entry on mac os if groovy is not on the path

--- Contents/MacOS/runAsApplication.sh
+++ Contents/MacOS/runAsApplication.sh
@@ -1,6 +1,14 @@
 #!/bin/sh -li
 
-echo "runAsApplication" > ~/test_app_run
+command -v groovy > /dev/null
+GROOVY_EXISTS=$?
+
+if test !$GROOVY_EXISTS; then
+    MSG="Command 'groovy' is not on the PATH! Is groovy installed on the system?";
+    echo "$MSG";
+    syslog -s -k Facility com.apple.console Sender JenkinsBell.app Level 3 Message "$MSG";
+    exit -1;
+fi
 
 cd "$(dirname $0)"
 DIR=$(pwd)
--- Contents/MacOS/start.sh
+++ Contents/MacOS/start.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -li
 
+command -v groovy > /dev/null
+GROOVY_EXISTS=$?
+
+if test !$GROOVY_EXISTS; then
+    MSG="Command 'groovy' is not on the PATH! Is groovy installed on the system?";
+    echo "$MSG";
+    syslog -s -k Facility com.apple.console Sender JenkinsBell.app Level 3 Message "$MSG";
+    exit -1;
+fi
+
 cd "$(dirname $0)"
 DIR=$(pwd)
 
